6b592252df20c115726ff12ca0ca2269666ae67a
nguyendong2003 <dongcoi14122003@gmail.com>
2024-05-29 17:56:11 +0700

    fix display correct data in editPost UI

--- screens/EditPostScreen.js
+++ screens/EditPostScreen.js
@@ -208,102 +208,104 @@ export default function EditPostScreen({ navigation, route }) {
             />
           </View>
 
-          <View
-            style={{
-              borderWidth: 1,
-              borderColor: '#ccc',
-              borderBottomWidth: 0,
-              marginHorizontal: 8,
-              paddingBottom: 8,
-            }}
-          >
+          {item?.share_post != null && (
             <View
               style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                padding: 12,
-                paddingBottom: 0,
+                borderWidth: 1,
+                borderColor: '#ccc',
+                borderBottomWidth: 0,
+                marginHorizontal: 8,
+                paddingBottom: 8,
               }}
             >
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate('Profile', {
-                    isPersonalPage: false,
-                    statusFriend: 'realFriend',
-                    listFriend: [],
-                  });
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  padding: 12,
+                  paddingBottom: 0,
                 }}
               >
-                <Image
-                  source={
-                    item.avatar == null
-                      ? require('../assets/defaultProfilePicture.jpg')
-                      : { uri: item.avatar }
-                  }
-                  style={{
-                    width: 40,
-                    height: 40,
-                    borderRadius: 100,
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate('Profile', {
+                      isPersonalPage: false,
+                      statusFriend: 'realFriend',
+                      listFriend: [],
+                    });
                   }}
-                  resizeMode="cover"
-                />
-              </TouchableOpacity>
-
-              <View style={{ marginLeft: 8 }}>
-                <TouchableOpacity>
-                  <Text
-                    style={{
-                      color: '#050505',
-                      fontSize: 15,
-                      fontWeight: '600',
-                    }}
-                  >
-                    {/* {item.profile_name} */}
-                    Nguyễn Đông
-                  </Text>
-                </TouchableOpacity>
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                  <Text
-                    style={{
-                      color: '#65676B',
-                      fontSize: 13,
-                      fontWeight: 400,
-                    }}
-                  >
-                    {moment(item.create_time, 'YYYY-MM-DD HH:mm:ss').format(
-                      'DD/MM/yyyy HH:mm'
-                    )}
-                  </Text>
-                  <Ionicons
-                    style={{ marginLeft: 12 }}
-                    name={
-                      item.view_mode === 'public'
-                        ? 'earth-sharp'
-                        : item.view_mode === 'friend'
-                        ? 'people-sharp'
-                        : 'lock-closed'
+                >
+                  <Image
+                    source={
+                      item.avatar == null
+                        ? require('../assets/defaultProfilePicture.jpg')
+                        : { uri: item.avatar }
                     }
-                    size={14}
-                    color="#050505"
+                    style={{
+                      width: 40,
+                      height: 40,
+                      borderRadius: 100,
+                    }}
+                    resizeMode="cover"
                   />
+                </TouchableOpacity>
+
+                <View style={{ marginLeft: 8 }}>
+                  <TouchableOpacity>
+                    <Text
+                      style={{
+                        color: '#050505',
+                        fontSize: 15,
+                        fontWeight: '600',
+                      }}
+                    >
+                      {item?.share_post?.user?.profile_name}
+                    </Text>
+                  </TouchableOpacity>
+                  <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Text
+                      style={{
+                        color: '#65676B',
+                        fontSize: 13,
+                        fontWeight: 400,
+                      }}
+                    >
+                      {moment(item.create_time, 'YYYY-MM-DD HH:mm:ss').format(
+                        'DD/MM/yyyy HH:mm'
+                      )}
+                    </Text>
+                    <Ionicons
+                      style={{ marginLeft: 12 }}
+                      name={
+                        item.view_mode === 'public'
+                          ? 'earth-sharp'
+                          : item.view_mode === 'friend'
+                          ? 'people-sharp'
+                          : 'lock-closed'
+                      }
+                      size={14}
+                      color="#050505"
+                    />
+                  </View>
                 </View>
               </View>
-            </View>
-            <View>
-              <Text
-                style={{
-                  marginTop: 8,
-                  fontSize: 17,
-                  fontWeight: '400',
+              <View>
+                <Text
+                  style={{
+                    marginTop: 8,
+                    fontSize: 17,
+                    fontWeight: '400',
 
-                  paddingHorizontal: 12,
-                  paddingVertical: 0,
-                }}
-              >
-                {item?.content}
-              </Text>
+                    paddingHorizontal: 12,
+                    paddingVertical: 0,
+                  }}
+                >
+                  {item?.share_post?.content}
+                </Text>
+              </View>
             </View>
-          </View>
+          )}
+
           <View
             style={{
               flexDirection: 'row',
