0fcaa2225f6bd76cb7a1f3ab4d07a45de58c770c
nguyendong2003 <dongcoi14122003@gmail.com>
2024-05-29 17:04:15 +0700

    update delete post in modal

--- components/Post.js
+++ components/Post.js
@@ -352,7 +352,7 @@ const Post = ({ item, navigation, onUpdatePost }) => {
                     alert('Delete post');
                   }}
                 >
-                  <Feather name="trash-2" size={36} color="red" />
+                  <Feather name="trash-2" size={24} color="#050505" />
                   <Text
                     style={{
                       fontSize: 32,
