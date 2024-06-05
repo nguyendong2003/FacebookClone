7c36c0e62ff412386e207e8cab2fe67920f6e88b
Hung22072003 <tranngocnhathung2207@gmail.com>
2024-06-05 15:58:57 +0700

    refactor service

--- service/NotificationService.js
+++ service/NotificationService.js
@@ -7,12 +7,27 @@ export const getNotificationByAccount = async(account_id) => {
     return response.data;
 }
 
-export const createNotification = async({from_account_id, to_account_id, to_post_id, to_comment_post_id, notify_type}) => {
+export const getSendNotificationFromFriendRequest = async(receiverId) => {
+  const response = await SpringServer.get(
+    `/facebook.api/notifications/getSendNotification/${receiverId}`
+  );
+  return response.data;
+}
+
+export const getReceiveNotificationFromFriendRequest = async(senderId) => {
+  const response = await SpringServer.get(
+    `/facebook.api/notifications/getReceiveNotification/${senderId}`
+  );
+  return response.data;
+}
+
+export const createNotification = async({from_account_id, to_account_id, to_post_id, to_comment_post_id, send_comment_id, notify_type}) => {
     const formData = new FormData();
     formData.append("from_account_id", from_account_id)
     formData.append("to_account_id", to_account_id)
     if(to_post_id != null) formData.append("to_post_id", to_post_id)
     if(to_comment_post_id != null) formData.append("to_comment_post_id", to_comment_post_id)
+    if(send_comment_id != null) formData.append("send_comment_id", send_comment_id)
     formData.append("notify_type", notify_type)
 
     const response = await SpringServer.post(
@@ -32,4 +47,11 @@ export const updateNotification = async(id_notify) => {
         `/facebook.api/notifications/update/${id_notify}`
     );
     return response.data;
+}
+
+export const deleteNotification = async(id_notify) => {
+    const response = await SpringServer.delete(
+      `/facebook.api/notifications/delete/${id_notify}`
+    );
+  return response.data;
 }
