9033d25d5203405b46491696bc82060b742549ce
Hung22072003 <tranngocnhathung2207@gmail.com>
2024-06-05 15:55:09 +0700

    add menu notification

--- components/Notification.js
+++ components/Notification.js
@@ -1,17 +1,20 @@
-import { Image ,View, Text, StyleSheet, TouchableOpacity } from "react-native"
-import { useEffect, useState } from "react";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Image ,View, Text, StyleSheet, TouchableOpacity, Modal, Pressable, Feather } from "react-native"
+import { useEffect, useState,useContext } from "react";
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import B from "../components/B";
-import {updateNotification} from "../service/NotificationService"
+import {deleteNotification, updateNotification} from "../service/NotificationService"
 import {getPostOfComment} from "../service/CommentService"
 import moment from "moment";
+import { Context as PostContext } from "../context/PostContext";
+import { Context as FriendContext } from '../context/FriendContext';
 export default function Notification(
-    {navigation, item}
+    {navigation, item, onDelete}
 ){
+    
+    const { state: postState, reloadPost } = useContext(PostContext);
     const [isRead, setIsRead] = useState(item._read)
     const [type, setType] = useState(item.type)
-    const [post, setPost] = useState(null)
-
+    const [isPressingMore, setIsPressingMore] = useState(false);
     const text = item.content;
     const parts = text.split("</B>");
     const user = parts[0].replace("<B>", "");
@@ -43,29 +46,15 @@ export default function Notification(
             case "SORRY":
                 return (<Image style={{width:24, height:24}} source={require('../iconfb/sad.png')}/>)
                 break;
+            case "friend_request":
+                return (<Image style={{width:32, height:32}} source={require('../iconfb/friend.png')}/>)
+                break;
             default:
                 break;
         }
     }
 
     const remainTime = (time)=> {
-        // const currentTime = new Date().getTime();
-        // const timePost = new Date(time).getTime();
-        // const remain = currentTime - timePost;
-        // const remainSecond = remain/1000;
-        // const remainMinute = remainSecond/60;
-        // const remainHour = remainMinute/60;
-        // const remainDay = remainHour/24;
-        // if(remainSecond < 60){
-        //     return Math.floor(remainSecond) + " seconds ago";
-        // }else if(remainMinute < 60){
-        //     return Math.floor(remainMinute) + " minutes ago";
-        // }else if(remainHour < 24){
-        //     return Math.floor(remainHour) + " hours ago";
-        // }else{
-        //     return Math.floor(remainDay) + " days ago";
-        // }
-
         const createDate = new Date(time);
         const now = new Date();
 
@@ -103,28 +92,42 @@ export default function Notification(
         }
     }
 
-    const getPostOfCommentHandler = async() => {
-        if(item.to_comment_post_id) {
-            try {
-                const responsse = await getPostOfComment(item?.to_comment_post_id)
-                setPost(responsse)
-            }catch(error) {
-                console.log(error);
-            }
+    const updatePostById = async (id) => {
+        await reloadPost(id);
+    };
+
+    const handleDeleteNotification = async() => {
+        try {
+            const response = await deleteNotification(item?.id)
+            onDelete(item?.id)
+        }catch(error) {
+            console.log(error);
         }
     }
 
-    useEffect(() => {
-        getPostOfCommentHandler()
-    }, [item])
+    const { acceptFriendRequest, getFriendRequests, rejectFriendRequest} = useContext(FriendContext);
+    const acceptClickHandler = () => {
+        acceptFriendRequest(item?.sender_id).then(() => {
+            handleDeleteNotification()
+        });
+    }
 
+    const rejectClickHandler = () => {
+        rejectFriendRequest(item?.sender_id).then(() => {
+            handleDeleteNotification()
+        }
+    )};
     return(
         <View style={[styles.container, {backgroundColor : isRead? "white": "#e9f2f7"}]}>
             <TouchableOpacity 
             style={styles.notifyContainer}
             onPress={()=> {
                 updateNotificationHandler()
-                navigation.navigate("PostDetail", {postId: (item.to_post_id != null) ? item.to_post_id : post.id, commentId: item.to_comment_post_id})
+                if(item.type == "friend_request") {
+                    navigation.navigate("Friend")
+                }else {
+                    navigation.navigate("PostDetail", {postId: item.to_post_id, commentId: item.send_comment_id, onUpdatePost: updatePostById})
+                }
             }}
             >
                 <View>
@@ -139,8 +142,166 @@ export default function Notification(
                 </View>
                 <View style={styles.contentContainer}>
                     <Text numberOfLines={3} style={styles.titleNotification}><B>{user}</B>{rest}</Text>
-                    <Text style={styles.dateNotfication}>{moment(item?.create_time).fromNow()}</Text>                    
+                    <Text style={styles.dateNotfication}>{moment(item?.create_time).fromNow()}</Text>          
+                    {item.type == "friend_request" && 
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                            marginHorizontal: 8,
+                            marginTop: 8,
+                        }}
+                    >
+                        <TouchableOpacity
+                            style={{
+                                flex: 1,
+                                backgroundColor: '#0866ff',
+                                padding: 6,
+                                borderRadius: 8,
+                            }}
+                            onPress={acceptClickHandler}
+                        >
+                            <Text
+                            style={{
+                                fontSize: 16,
+                                color: 'white',
+                                textAlign: 'center',
+                                fontWeight: '500',
+                            }}
+                            >
+                            Accept
+                            </Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            style={{
+                            flex: 1,
+                            backgroundColor: '#e4e6eb',
+                            padding: 6,
+                            borderRadius: 8,
+                            marginLeft: 8,
+                            }}
+                            onPress={rejectClickHandler}
+                        >
+                            <Text
+                            style={{
+                                fontSize: 16,
+                                color: '#050505',
+                                textAlign: 'center',
+                                fontWeight: '500'
+                            }}
+                            >
+                            Delete
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                    }          
                 </View>
+
+                <View style = {{
+                    flexDirection:'column',
+                    justifyContent: 'center',
+                }}>
+                    <TouchableOpacity
+                        style={{
+                            padding: 8,
+                        }}
+                        onPress={() => setIsPressingMore(!isPressingMore)}
+                        >
+                        <MaterialIcons name="more-horiz" size={24} color="#65676B" />
+                    </TouchableOpacity>
+                </View>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={isPressingMore}
+                    onRequestClose={() => {
+                    // Alert.alert('Modal has been closed.');
+                    setIsPressingMore(!isPressingMore);
+                    }}
+                >
+                    <View
+                    style={{
+                        flex: 1,
+                        justifyContent: "flex-end",
+                        alignItems: "center",
+                        backgroundColor: "rgba(0,0,0,0.5)",
+                    }}
+                    >
+                    <Pressable
+                        style={{
+                        height: "75%",
+                        width: "100%",
+                        }}
+                        onPress={() => setIsPressingMore(false)}
+                    />
+                    <View
+                        style={{
+                        height: "25%",
+                        width: "100%",
+                        backgroundColor: "white",
+                        borderTopLeftRadius: 20,
+                        borderTopRightRadius: 20,
+                        padding: 20,
+                        }}
+                    >
+                        <View style = {{
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}>
+                            <View>
+                                <Image
+                                    style={styles.avartarNotify}
+                                    source={{uri: item?.avatar_sender}}
+                                />
+                                <View style={styles.commentIconContainer}>
+                                    {renderIcon(item.type)}
+                                </View>
+                            
+                            </View>
+                            <View style = {{
+                                marginTop: 5,
+                                marginBottom: 10
+                            }}>
+                                <Text numberOfLines={3} style={styles.titleNotification}>{user}{rest}</Text>
+                            </View>
+                        </View>
+
+                        <TouchableOpacity
+                            style={{
+                                flexDirection: "row",
+                                alignItems: "center",
+                                paddingVertical: 12,
+                                borderTopColor: "#ccc",
+                                borderTopWidth: 1,
+                            }}
+                            onPress={handleDeleteNotification}
+                            >
+                            <View style = {{
+                                width: 40,
+                                height: 40,
+                                borderRadius: 32,
+                                backgroundColor: "#e4e6eb",
+                                justifyContent: "center",
+                                alignItems:"center"
+                            }}>
+                                <Image style={{width:20, height:20}} source={require('../iconfb/remove.png')}/>
+                            </View> 
+                            <Text
+                                style={{
+                                fontSize: 18,
+                                color: "black",
+                                fontWeight: "500",
+                                marginLeft: 12,
+                                }}
+                            >
+                            Remove this notification
+                            </Text>
+                        </TouchableOpacity>
+                        
+                    </View>
+                </View>
+                </Modal>
             </TouchableOpacity>
         </View>
 
@@ -149,7 +310,7 @@ export default function Notification(
 
 const styles = StyleSheet.create({
     commentIconContainer:{
-        backgroundColor: "green",
+        backgroundColor: "#3dca60",
         width: 24,
         height: 24,
         justifyContent:"center",
@@ -180,6 +341,7 @@ const styles = StyleSheet.create({
         // margin: 10
     },
     contentContainer: {
+        width: "85%",
         flexDirection:"column",
         justifyContent: "center",
         // flexWrap: "wrap"
